b085e153e6b2797ef48ee55f745d6f3c27db6972
Nevil Patel <142852802+NevilPatel01@users.noreply.github.com>
2025-04-15 21:45:06 -0400

    Feat: added comment to Message.kt

--- app/src/main/java/ca/mohawk/temi8/Message.kt
+++ app/src/main/java/ca/mohawk/temi8/Message.kt
@@ -1,3 +1,9 @@
 package ca.mohawk.temi8
 
+/**
+ * A simple data class representing a message in the chat.
+ * @property text The actual text of the message.
+ * @property isUser Indicates whether this message was sent by the user (true) or by the assistant (false).
+ */
+
 data class Message(val text: String, val isUser: Boolean)
